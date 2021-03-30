b48f16e4919b403d45f508191c9724d7d5a1facf
lifanova <veralifanovaa@gmail.com>
2021-03-30 15:54:34 +0300

    Test methods and Jacoco configuration are added

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -1,6 +1,11 @@
 package ru.netology.radio;
 
 public class Radio {
+    private static final int MIN_STATION_NUMBER = 0;
+    private static final int MAX_STATION_NUMBER = 9;
+    private static final int MIN_VOLUME = 0;
+    private static final int MAX_VOLUME = 10;
+
     // Номер текущей (прослушиваемой) радиостанции
     private int stationNumber;
     // Громкость звука
@@ -11,10 +16,10 @@ public class Radio {
     }
 
     public void setStationNumber(int stationNumber) {
-        if(stationNumber > 9) {
-            stationNumber = 0;
-        } else if(stationNumber < 0) {
-            stationNumber = 9;
+        if(stationNumber > MAX_STATION_NUMBER) {
+            stationNumber = MIN_STATION_NUMBER;
+        } else if(stationNumber < MIN_STATION_NUMBER) {
+            stationNumber = MAX_STATION_NUMBER;
         }
 
         this.stationNumber = stationNumber;
@@ -25,10 +30,10 @@ public class Radio {
     }
 
     public void setVolume(int volume) {
-        if(volume > 10) {
-            volume = 10;
-        } else if(volume < 0) {
-            volume = 0;
+        if(volume > MAX_VOLUME) {
+            volume = MAX_VOLUME;
+        } else if(volume <MIN_VOLUME) {
+            volume = MIN_VOLUME;
         }
 
         this.volume = volume;
---
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -0,0 +1,79 @@
+package ru.netology.radio;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RadioTest {
+
+    @Test
+    void setStationNumberInRangeTest(){
+        Radio radio = new Radio();
+        // Проверяем любой положительный сценарий: номер в диапазоне [0; 9]
+        int number = 5;
+        radio.setStationNumber(number);
+
+        assertEquals(number, radio.getStationNumber());
+    }
+
+    @Test
+    void setStationNumberLessMinTest(){
+        Radio radio = new Radio();
+        // Проверяем, если номер радиостанции < MIN
+        int number = -1;
+        // Ожидаемый результат
+        int actual = 9;
+
+        radio.setStationNumber(number);
+
+        assertEquals(actual, radio.getStationNumber());
+    }
+
+    @Test
+    void setStationNumberMoreMaxTest(){
+        Radio radio = new Radio();
+        // Проверяем, если номер радиостанции > MAX
+        int number = 10;
+        // Ожидаемый результат
+        int actual = 0;
+
+        radio.setStationNumber(number);
+
+        assertEquals(actual, radio.getStationNumber());
+    }
+
+    @Test
+    void setVolumeInRangeTest(){
+        Radio radio = new Radio();
+        // Проверяем любой положительный сценарий: громкость в диапазоне [0; 10]
+        int volume = 5;
+        radio.setVolume(volume);
+
+        assertEquals(volume, radio.getVolume());
+    }
+
+    @Test
+    void setVolumeLessMinTest(){
+        Radio radio = new Radio();
+        // Проверяем, если громкость < MIN
+        int volume = -1;
+        // Ожидаемый результат
+        int actual = 0;
+
+        radio.setVolume(volume);
+
+        assertEquals(actual, radio.getVolume());
+    }
+
+    @Test
+    void setVolumeMoreMaxTest(){
+        Radio radio = new Radio();
+        // Проверяем, если громкость > MAX
+        int volume = 11;
+        // Ожидаемый результат
+        int actual = 10;
+
+        radio.setVolume(volume);
+
+        assertEquals(actual, radio.getVolume());
+    }
+}
